edfebfffbfbd03f475652dd9c4bbc0a905f05fba
mingming <jimin23623@gmail.com>
2024-01-21 18:19:39 +0900

    ✨ feat: useAsync 기능 추가

---
+++ src/hooks/useAsync.js
@@ -0,0 +1,29 @@
+import { useCallback, useState } from 'react';
+
+// 로딩과 에러 메시지를 적을 상태 저장
+// getAsyncFunction에 원하는 api 호출 함수가 들어가야한다.
+
+export const useAsync = (getAsyncFunction) => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const setAsyncFunction = useCallback(
+    async (...args) => {
+      try {
+        setError(null);
+        setLoading(true);
+
+        return await getAsyncFunction(...args);
+      } catch (error) {
+        setError(error);
+
+        return;
+      } finally {
+        setLoading(false);
+      }
+    },
+    [getAsyncFunction],
+  );
+
+  return [loading, error, setAsyncFunction];
+};
