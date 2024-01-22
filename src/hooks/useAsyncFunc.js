a03842547de699b9b7c2b913a89b1c1ba74e3355
mingming <jimin23623@gmail.com>
2024-01-22 15:58:44 +0900

    🧹 chore: 컴포넌트 이름 잘못 들어간 것 수정 완료

--- src/hooks/useAsyncFunc.js
+++ src/hooks/useAsyncFunc.js
@@ -4,7 +4,7 @@ import { useCallback, useState } from 'react';
 // getAsyncFunction에 원하는 api 호출 함수가 들어가야한다.
 // 특정 이벤트 발생했을 때 사용할 수 있는 커스텀 훅이다.
 
-export const useAsync = (getAsyncFunction) => {
+export const useAsyncFunc = (getAsyncFunction) => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
