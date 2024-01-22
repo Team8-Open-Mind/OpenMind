5580482480fd411556943531e81eff5088d76e28
Vegatality <vegatality@gmail.com>
2024-01-22 22:51:15 +0900

    ✨ #root div에 min-height 100vh 주고 배경색 설정

--- src/pages/list-page/ListPage.jsx
+++ src/pages/list-page/ListPage.jsx
@@ -1,45 +1,35 @@
+import { useNavigate } from 'react-router-dom';
+
 import { css } from 'styled-components';
 
 import Button from '@components/ui/atoms/Button/Button';
-import ArrowDropdown from '@components/ui/atoms/dropdown/arrow-dropdown/ArrowDropdown';
-import HamburgerDropdown from '@components/ui/atoms/dropdown/hamburger-dropdown/HamburgerDropdown';
 import Logo from '@components/ui/atoms/logo/Logo';
 
 const ListPage = () => {
+  const navigate = useNavigate();
+
   return (
-    <>
-      <div
+    <div
+      css={css`
+        padding: 4rem 5rem;
+      `}
+    >
+      <nav
         css={css`
-          padding: 4rem 5rem 0;
-        `}
-      >
-        <nav
-          css={css`
-            max-width: 94rem;
-            height: 5.7rem;
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            margin-inline: auto;
-          `}
-        >
-          <Logo />
-          <Button theme='brown20' arrow>
-            답변하러 가기
-          </Button>
-        </nav>
-      </div>
-      <div
-        css={css`
+          max-width: 94rem;
+          height: 5.7rem;
           display: flex;
-          width: 100%;
           justify-content: space-between;
+          align-items: center;
+          margin-inline: auto;
         `}
       >
-        <ArrowDropdown />
-        <HamburgerDropdown />
-      </div>
-    </>
+        <Logo onClickHandler={() => navigate('/')} />
+        <Button theme='brown20' arrow>
+          답변하러 가기
+        </Button>
+      </nav>
+    </div>
   );
 };
 
