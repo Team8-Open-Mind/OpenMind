d78be22728851970c0605be84156b5f4e8157ad9
mingming <jimin23623@gmail.com>
2024-01-22 15:46:11 +0900

    ✨ feat: useAsync 렌더링 바로 호출되는 비동기 훅 추가

--- src/hooks/useAsyncFunc.js
+++ src/hooks/useAsyncFunc.js
@@ -1,13 +1,12 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useState } from 'react';
 
 // 로딩과 에러 메시지를 적을 상태 저장
 // getAsyncFunction에 원하는 api 호출 함수가 들어가야한다.
-// 특정 이벤트 발생 없을때도 바로 받아 올 수 있다.
+// 특정 이벤트 발생했을 때 사용할 수 있는 커스텀 훅이다.
 
 export const useAsync = (getAsyncFunction) => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
-  const [result, setResult] = useState();
 
   const setAsyncFunction = useCallback(
     async (...args) => {
@@ -15,11 +14,7 @@ export const useAsync = (getAsyncFunction) => {
         setError(null);
         setLoading(true);
 
-        const data = await getAsyncFunction(...args);
-
-        setResult(data);
-
-        return data;
+        return await getAsyncFunction(...args);
       } catch (error) {
         setError(error);
 
@@ -31,10 +26,5 @@ export const useAsync = (getAsyncFunction) => {
     [getAsyncFunction],
   );
 
-  useEffect(() => {
-    // 렌더링되자마자 호출되도록
-    setAsyncFunction();
-  }, [setAsyncFunction]);
-
-  return [loading, error, result];
+  return [loading, error, setAsyncFunction];
 };
--- src/pages/list-page/ListPage.jsx
+++ src/pages/list-page/ListPage.jsx
@@ -1,4 +1,17 @@
+import { useEffect } from 'react';
+
+import { getTotalSubject } from '@api/getTotalSubject';
+import { useAsync } from '@hooks/useAsync';
+
 const ListPage = () => {
+  const [loading, error, result] = useAsync(getTotalSubject);
+
+  useEffect(() => {
+    if (result) {
+      console.log(result);
+    }
+  }, [result]);
+
   return <div>ListPage</div>;
 };
 
