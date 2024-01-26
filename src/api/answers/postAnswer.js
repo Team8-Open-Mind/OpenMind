931caa3e9fbe19af86806d0bc4218a22e80409aa
mingming <jimin23623@gmail.com>
2024-01-26 20:46:11 +0900

    🧹 chore: 주석삭제

--- src/api/answers/postAnswer.js
+++ src/api/answers/postAnswer.js
@@ -10,6 +10,6 @@ export const postAnswer = async (questionId, content, isRejected) => {
     return response;
   } catch (error) {
     console.error('에러 발생:', error);
-    throw error; // 에러를 다시 throw하여 호출자에게 전파
+    throw error;
   }
 };
