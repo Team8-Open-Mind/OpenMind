c904c2a4535097685773866a7e9f058f633f9426
mingming <jimin23623@gmail.com>
2024-01-26 20:45:21 +0900

    🧹 chore: 주석 삭제

--- src/api/answers/postAnswer.js
+++ src/api/answers/postAnswer.js
@@ -2,8 +2,6 @@ import axiosInstance from '@api/instance/axiosInstance';
 
 export const postAnswer = async (questionId, content, isRejected) => {
   try {
-    // 파라미터 값들을 잘 받아오고 있음을 확인하였음
-    // console.log(questionId, content, isRejected);
     const response = await axiosInstance.post(`/questions/${questionId}/answers/`, {
       content,
       isRejected,
