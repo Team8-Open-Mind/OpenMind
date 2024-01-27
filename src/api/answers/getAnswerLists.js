5f5296647836804a0169fb9865e43b0a16898735
Vegatality <vegatality@gmail.com>
2024-01-27 12:20:49 +0900

    🐛 fix: AnswerPage, QuestionFeedPage 리렌더링 되도록 수정

--- src/api/answers/getAnswerLists.js
+++ src/api/answers/getAnswerLists.js
@@ -1,12 +1,14 @@
 import axiosInstance from '@api/instance/axiosInstance';
 
-export const getAnswerLists = async () => {
-  const response = await axiosInstance.get(`/subjects/2756/questions/`, {
+export const getAnswerLists = async ({ userId }) => {
+  const response = await axiosInstance.get(`/subjects/${userId}/questions/`, {
+    // * limit 기본값 8개
     params: {
-      limit: 8,
+      limit: 10,
       offset: 0,
     },
   });
+  // const response = await axiosInstance.get(`/subjects/${userId}/questions/`);
 
   if (response.status !== 200) {
     throw new Error(`질문 리스트를 불러오는데 실패하였어요!`);
--- src/hooks/useSetUser.js
+++ src/hooks/useSetUser.js
@@ -2,34 +2,49 @@ import { useEffect, useState } from 'react';
 
 import PROFILE_SAMPLE from '@components/ui/atoms/profile-sample';
 
-import getUserData from '@api/getUserData';
+import getUserData from '@api/subjects/getUserData';
 
-const useSetUser = ({ selectedId = 2750 }) => {
-  const [userName, setUserName] = useState('');
-  const [userProfile, setUserProfile] = useState(PROFILE_SAMPLE);
-  const [createdAt, setCreatedAt] = useState('');
-  const [questionCount, setQuestionCount] = useState(0);
+const useSetUser = ({ userId = 2750 }) => {
+  // const [userName, setUserName] = useState('');
+  // const [userProfile, setUserProfile] = useState(PROFILE_SAMPLE);
+  // const [createdAt, setCreatedAt] = useState('');
+  // const [questionCount, setQuestionCount] = useState(0);
+
+  const [userInfo, setUserInfo] = useState({
+    name: '',
+    imageSource: PROFILE_SAMPLE,
+    createdAt: '',
+    questionCount: 0,
+    id: null,
+  });
 
   useEffect(() => {
     const fetchUserData = async () => {
       try {
-        const userData = await getUserData();
-        const selectedUser = userData.find((value) => value.id === selectedId);
+        const { name, imageSource, createdAt, questionCount, id } = await getUserData(userId);
+        setUserInfo({
+          name,
+          imageSource,
+          createdAt,
+          questionCount,
+          id,
+        });
+        // const selectedUser = userData.find((value) => value.id === userId);
 
-        if (selectedUser) {
-          setUserName(selectedUser.name);
-          setUserProfile(selectedUser.imageSource);
-          setCreatedAt(selectedUser.createdAt);
-          setQuestionCount(selectedUser.questionCount);
-        }
+        // if (selectedUser) {
+        //   setUserName(selectedUser.name);
+        //   setUserProfile(selectedUser.imageSource);
+        //   setCreatedAt(selectedUser.createdAt);
+        //   setQuestionCount(selectedUser.questionCount);
+        // }
       } catch (error) {
         console.error('데이터를 가져오는 중 오류가 발생했습니다:', error);
       }
     };
     fetchUserData();
-  }, [selectedId]);
+  }, [userId]);
 
-  return { userName, userProfile, createdAt, questionCount };
+  return { userInfo };
 };
 
 export default useSetUser;
