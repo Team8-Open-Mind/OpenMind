cae38660770de54cd0a869942098cbeca87fa3c5
mingming <jimin23623@gmail.com>
2024-01-27 14:36:48 +0900

    🧹 chore: api 문구 및 에러 코드 수정

--- src/api/answers/postAnswer.js
+++ src/api/answers/postAnswer.js
@@ -7,6 +7,10 @@ export const postAnswer = async (questionId, content, isRejected) => {
       isRejected,
     });
 
+    if (response.status !== 201) {
+      throw new Error(`답변 남기는데 실패하였어요!`);
+    }
+
     return response;
   } catch (error) {
     console.error('에러 발생:', error);
--- src/api/questions/deleteQuestion.js
+++ src/api/questions/deleteQuestion.js
@@ -3,8 +3,8 @@ import axiosInstance from '@api/instance/axiosInstance';
 const deleteQuestion = async (id) => {
   const response = await axiosInstance.delete(`/questions/${id}/`);
 
-  if (response.status !== 200) {
-    throw new Error(`답변을 삭제하는데 실패하였어요!`);
+  if (response.status !== 204) {
+    throw new Error(`질문을 삭제하는데 실패하였어요!`);
   }
 
   return response;
