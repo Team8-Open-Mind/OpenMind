bd1a4a60ab79b73cf8321acfcfff3cc314ee0d9b
mingming <jimin23623@gmail.com>
2024-01-27 16:08:09 +0900

    ✨ feat: notFound 추가

--- src/pages/not-found-page/NotFoundPage.jsx
+++ src/pages/not-found-page/NotFoundPage.jsx
@@ -1,3 +1,6 @@
+import { useNavigate } from 'react-router-dom';
+
+import { device } from '@device/mediaBreakpoints';
 import styled from 'styled-components';
 
 import Button from '@components/ui/atoms/Button/Button';
@@ -5,6 +8,12 @@ import Logo from '@components/ui/atoms/logo/Logo';
 import { StBackground } from '@pages/question-feed-page/QuestFeedPage';
 
 const NotFoundPage = () => {
+  const navigate = useNavigate();
+
+  const handleRedirectClick = () => {
+    navigate('/');
+  };
+
   return (
     <div>
       <StBackground>
@@ -15,7 +24,7 @@ const NotFoundPage = () => {
             페이지가 존재하지 않거나, 사용할 수 없는 페이지입니다. <br />
             입력하신 주소를 다시 한 번 확인해 주세요
           </StErrorDescribe>
-          <Button theme='brown20' arrow>
+          <Button theme='brown20' arrow onClickHandler={handleRedirectClick}>
             메인 페이지로 이동하기
           </Button>
         </StMainWrapper>
@@ -38,17 +47,27 @@ const StMainWrapper = styled.div`
 `;
 
 const StError = styled.h2`
-  font-size: 4rem;
+  font-size: 2.8rem;
   letter-space: -0.024em;
-  line-height: 42px;
+  line-height: 30px;
   color: ${({ theme }) => theme.color.Brown[40]};
   text-align: center;
+
+  @media ${device.tablet} {
+    font-size: 4rem;
+    line-height: 42px;
+  }
 `;
 
 const StErrorDescribe = styled.p`
-  font-size: 2.4rem;
-  line-height: 34px;
+  font-size: 1.6rem;
+  line-height: 20px;
   color: ${({ theme }) => theme.color.Brown[40]};
   text-align: center;
-  margin-bottom: 20px;
+  margin-bottom: 10px;
+
+  @media ${device.tablet} {
+    font-size: 2rem;
+    line-height: 30px;
+  }
 `;
