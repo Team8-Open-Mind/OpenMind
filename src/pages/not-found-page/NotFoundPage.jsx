cf40da28a75715b96023a134e48b58cbbb970ee0
mingming <jimin23623@gmail.com>
2024-01-27 15:45:07 +0900

    ✨ feat: NotFoundPage 추가

--- src/pages/not-found-page/NotFoundPage.jsx
+++ src/pages/not-found-page/NotFoundPage.jsx
@@ -1,5 +1,54 @@
+import styled from 'styled-components';
+
+import Button from '@components/ui/atoms/Button/Button';
+import Logo from '@components/ui/atoms/logo/Logo';
+import { StBackground } from '@pages/question-feed-page/QuestFeedPage';
+
 const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
+  return (
+    <div>
+      <StBackground>
+        <StMainWrapper>
+          <Logo width='462px' height='180px' />
+          <StError>404 Error</StError>
+          <StErrorDescribe>
+            페이지가 존재하지 않거나, 사용할 수 없는 페이지입니다. <br />
+            입력하신 주소를 다시 한 번 확인해 주세요
+          </StErrorDescribe>
+          <Button theme='brown20' arrow>
+            메인 페이지로 이동하기
+          </Button>
+        </StMainWrapper>
+      </StBackground>
+    </div>
+  );
 };
 
 export default NotFoundPage;
+
+const StMainWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 25px;
+  width: 100%;
+  height: 100vh;
+  margin: -70px auto;
+`;
+
+const StError = styled.h2`
+  font-size: 4rem;
+  letter-space: -0.024em;
+  line-height: 42px;
+  color: ${({ theme }) => theme.color.Brown[40]};
+  text-align: center;
+`;
+
+const StErrorDescribe = styled.p`
+  font-size: 2.4rem;
+  line-height: 34px;
+  color: ${({ theme }) => theme.color.Brown[40]};
+  text-align: center;
+  margin-bottom: 20px;
+`;
