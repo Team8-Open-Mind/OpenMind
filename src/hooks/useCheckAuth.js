a7b4b80c00cf54eae3d3eb76af595f39d4a8060d
mingming <jimin23623@gmail.com>
2024-01-28 14:07:05 +0900

    🐛 fix: 렌더링 여러번 되어서 오류나는 부분 수정

--- src/hooks/useCheckAuth.js
+++ src/hooks/useCheckAuth.js
@@ -1,20 +1,22 @@
 import { useEffect, useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 export const useCheckAuth = () => {
-  const { userId } = useParams(); // React Router의 useParams를 이용해 URL에서 userId를 추출
-  const [isUser, setIsUser] = useState(false);
-  const storedUserId = localStorage.getItem('userId');
+  const location = useLocation();
   const navigate = useNavigate();
+  const userIdMatch = location.pathname.match(/\/post\/(\d+)\/answer/);
+  const userId = userIdMatch ? userIdMatch[1] : null;
+  const storedUserId = localStorage.getItem('userId');
+  const [isUser, setIsUser] = useState(false);
 
+  // userId와 storedUerId 값이 둘 다 존재하는데 두 값이 서로 다를 경우
   useEffect(() => {
-    // userId와 storedUerId 값이 둘 다 존재하는데 두 값이 서로 다를 경우
-    if (userId && storedUserId && userId !== storedUserId) {
+    if (userId !== storedUserId) {
       setIsUser(false);
     } else {
       setIsUser(true);
     }
-  }, [userId, storedUserId]);
+  }, [storedUserId, userId]);
 
   return { isUser, navigate };
 };
