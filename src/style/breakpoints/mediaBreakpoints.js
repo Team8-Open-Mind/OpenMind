9fcc38d622f2b95341e13ec0133e62df3a04093f
jihee <sweet9811@gmail.com>
2024-01-28 15:43:37 +0900

    🐛 fix: 미디어쿼리 태블릿 적용너비 변경 및 메인페이지 반응형 적용

--- src/style/breakpoints/mediaBreakpoints.js
+++ src/style/breakpoints/mediaBreakpoints.js
@@ -1,11 +1,11 @@
 // mobile first develop: default mobile
 export const size = {
   tablet: 768,
-  pc: 1200,
+  pc: 1025,
 };
 
 /**
- * - PC: 1200px 이상
+ * - PC: 1025px 이상
  * - Tablet: 768px 이상 ~ 1199px 이하
  * - Mobile: 375px 이상 ~ 767px 이하
  * * `375px 미만 사이즈의 디자인은 고려하지 않습니다`
