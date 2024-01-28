ebfd3dc86d7967feed2177a456e6884e9f170d68
Vegatality <vegatality@gmail.com>
2024-01-29 02:35:36 +0900

    test: dropdown 원격 배포에서 boolean 뭐 찍히는 지 확인하기 위한 커밋

--- src/components/ui/atoms/dropdown/DropdownMenu.jsx
+++ src/components/ui/atoms/dropdown/DropdownMenu.jsx
@@ -6,6 +6,8 @@ import { useDropdownProvider } from './context/DropdownProvider';
 const DropdownMenu = ({ children, positionTop = '38', positionRight, width }) => {
   const { isOpen } = useDropdownProvider();
 
+  console.log(isOpen);
+
   return (
     <>
       <StDropdownMenu $isOpen={isOpen} $width={width} $positionTop={positionTop} $positionRight={positionRight}>
