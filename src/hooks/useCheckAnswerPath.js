f14cc07f6f0cd91be6e6fad8dfc869414cbfed75
mingming <jimin23623@gmail.com>
2024-01-28 14:07:05 +0900

    🐛 fix: 렌더링 여러번 되어서 오류나는 부분 수정

--- src/hooks/useCheckAnswerPath.js
+++ src/hooks/useCheckAnswerPath.js
@@ -1,18 +1,31 @@
 import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
+import { useCheckAuth } from './useCheckAuth';
+
 export const useCheckAnswerPath = () => {
   const location = useLocation();
   const [path, setPath] = useState('');
+  const { isUser, navigate } = useCheckAuth();
 
   // 현재 URL에 '/answer'가 포함되어 있는지 여부를 반환
   useEffect(() => {
     if (location.pathname.includes('/answer')) {
+      // 첨에 false 두 번 나오고 true가 나와서 첫 false에서 alert가 떠버림
       setPath('answer');
     } else {
       setPath('question');
     }
-  }, [location.pathname, path]);
+  }, [location.pathname, isUser]);
+
+  useEffect(() => {
+    if (path === 'answer') {
+      if (!isUser) {
+        navigate('/');
+        alert('접근권한이 없습니다.');
+      }
+    }
+  });
 
   return { path };
 };
