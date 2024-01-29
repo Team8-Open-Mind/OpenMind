7f72db471335334d3da79d25f2dc4b433e2b0160
Vegatality <vegatality@gmail.com>
2024-01-29 17:52:48 +0900

    perf: useAsyncFn_V2에 queue로 처리하도록 로직 강화

--- src/hooks/useAsyncFn_V2.js
+++ src/hooks/useAsyncFn_V2.js
@@ -1,44 +1,44 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useRef, useState } from 'react';
 
-import useMountedState from '@hooks/useMountedState';
+import { useMountedState } from '@hooks/useMountedState';
 
 const useAsyncFn_V2 = ({ asyncFn, onSuccess, onError, deps = [] }) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [data, setData] = useState(null);
-
-  const [{ isError, error }, setErrorState] = useState({
+  const [{ data, error, isError, isLoading }, setState] = useState({
+    isLoading: false,
+    data: null,
     isError: false,
     error: new Error(),
   });
+
+  // 맨 마지막 요청만 처리
+  const queueRef = useRef(0);
   const isMounted = useMountedState();
 
   const setAsyncFunction = useCallback(async (...args) => {
-    try {
-      if (!isLoading) setIsLoading(true);
+    const lastIndexOfQueue = ++queueRef.current;
 
-      const res = await asyncFn(...args);
+    return (async () => {
+      try {
+        if (!isLoading) setState((prev) => ({ ...prev, isLoading: true }));
 
-      // Todo: res가 undefined일 때도 일단은 onSuccess를 호출하도록 해야 할까? 고민.
-      // if (isMounted() && onSuccess && typeof onSuccess === 'function' && res) onSuccess(res);
-      if (isMounted() && onSuccess && typeof onSuccess === 'function') {
-        onSuccess(res);
-        setData(res);
+        const res = await asyncFn(...args);
+
+        // Todo: res가 undefined일 때도 일단은 onSuccess를 호출하도록 해야 할까? 고민.
+        if (isMounted() && queueRef.current === lastIndexOfQueue && onSuccess && typeof onSuccess === 'function') {
+          onSuccess(res);
+          setState({ data: res, isLoading: false, isError: false });
+        }
+
+        return res;
+      } catch (error) {
+        if (isMounted() && queueRef.current === lastIndexOfQueue && onError && typeof onError === 'function') {
+          onError(error);
+          setState({ error, isLoading: false, isError: true });
+        }
+
+        return error;
       }
-
-      return res;
-    } catch (error) {
-      if (isMounted() && onError && typeof onError === 'function') {
-        onError(error);
-        setErrorState({
-          isError: true,
-          error,
-        });
-      }
-
-      return error;
-    } finally {
-      setIsLoading(false);
-    }
+    })();
 
     // * 매번 새로 할당되는 익명함수도 받을 수 있게 하기 위해 아래와 같이 eslint 끔. deps가 없을 때는 빈 배열을 넣어줌.
     // eslint-disable-next-line react-hooks/exhaustive-deps
