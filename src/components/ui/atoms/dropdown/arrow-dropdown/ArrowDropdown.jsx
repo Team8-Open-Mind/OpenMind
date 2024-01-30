b3ab6337e30ddccbab41df888f40ff77c2def9d9
Vegatality <vegatality@gmail.com>
2024-01-30 16:02:34 +0900

    ♻️ refactor: dropdown 데이터 로직 UI랑 분리

--- src/components/ui/atoms/dropdown/arrow-dropdown/ArrowDropdown.jsx
+++ src/components/ui/atoms/dropdown/arrow-dropdown/ArrowDropdown.jsx
@@ -2,13 +2,11 @@ import Dropdown from '..';
 
 const ArrowDropdown = ({ callbackFn }) => {
   return (
-    <Dropdown>
+    <Dropdown callbackFn={callbackFn}>
       <Dropdown.Select />
       <Dropdown.Menu width='7.9rem'>
-        <Dropdown.Option callbackFn={callbackFn}>이름순</Dropdown.Option>
-        <Dropdown.Option selected callbackFn={callbackFn}>
-          최신순
-        </Dropdown.Option>
+        <Dropdown.Option>이름순</Dropdown.Option>
+        <Dropdown.Option selected>최신순</Dropdown.Option>
       </Dropdown.Menu>
     </Dropdown>
   );
