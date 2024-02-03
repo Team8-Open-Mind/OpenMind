2d48f4c7069e66ea0f82c8f413856559a21d6881
mingming <jimin23623@gmail.com>
2024-02-03 19:39:30 +0900

    ♻️ refactor: 좋아요 기능 코드 간결하게 수정

--- src/components/ui/atoms/card/CardLikeButton.jsx
+++ src/components/ui/atoms/card/CardLikeButton.jsx
@@ -2,13 +2,25 @@ import styled from 'styled-components';
 
 import { ReactComponent as ThumbsUp } from '@assets/thumbs-up.svg';
 
+import { postReaction } from '@api/questions/postReaction';
+import { useAsync } from '@hooks/useAsync';
+
 import { useCardProvider } from './context/cardProvider';
 
 const CardLikeButton = () => {
-  const { cardData } = useCardProvider();
+  const { cardData, setRequestType } = useCardProvider();
+  const questionId = cardData?.id;
+  const { setAsyncFunction } = useAsync(postReaction);
+
+  const handleReactionCount = async () => {
+    const res = await setAsyncFunction(questionId);
+    setRequestType('reaction');
+
+    return res;
+  };
 
   return (
-    <StReaction>
+    <StReaction onClick={handleReactionCount}>
       <StThumbsUp width='14px' height='14px' />
       <span>도움이 되었어요</span>
       <span>{cardData?.like}</span>
